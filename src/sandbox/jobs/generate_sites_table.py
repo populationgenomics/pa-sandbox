7d9660e9d8b6fe74ff5839365d174fd28b3f193d
Alex Stuckey <13140057+Alexander-Stuckey@users.noreply.github.com>
2025-07-10 15:59:30 +1200

    swap to calculating inbreeding as per gnomad (#20)

--- src/sandbox/jobs/generate_sites_table.py
+++ src/sandbox/jobs/generate_sites_table.py
@@ -5,6 +5,7 @@ from cpg_flow.targets import Cohort
 from cpg_flow.utils import to_path  # type: ignore[ReportUnknownVariableType]
 from cpg_utils.config import config_retrieve, genome_build, get_driver_image
 from cpg_utils.hail_batch import get_batch, init_batch, output_path  # type: ignore[ReportUnknownVariableType]
+from gnomad_methods.bi_allelic_sites_inbreeding import bi_allelic_site_inbreeding_expr
 from hailtop.batch.job import PythonJob, PythonResult
 from loguru import logger
 
@@ -158,9 +159,8 @@ def _run_sites_per_chromosome(cohort_name: str, chromosome: str) -> str:  # noqa
 
             logger.info('Done running variant QC. Now generating sites table and filtering using gnomAD v3 parameters')
             cohort_dense_mt = cohort_dense_mt.annotate_rows(
-                IB=hl.agg.inbreeding(
+                IB=bi_allelic_site_inbreeding_expr(
                     cohort_dense_mt.GT,
-                    cohort_dense_mt.variant_qc.AF[1],
                 ),
             )
             cohort_dense_mt = cohort_dense_mt.filter_rows(
